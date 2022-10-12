bf43f7b918b9612ba2e4a43a47aefb363f32914d
jusgu <juustin.gu@gmail.com>
2022-10-12 02:27:58 -0400

    added sidebar and ui improvements

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import './App.css';
-import { Title, Text, Box } from '@mantine/core';
+import { Title, Text, Box, Modal, Accordion } from '@mantine/core';
 import Messages from './messages';
+import { IconQuestionMark, IconBrandGithub, IconChevronRight } from '@tabler/icons';
+import { useState } from 'react';
 
 function App() {
+  const [modalOpened, setModalOpened] = useState(false);
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
   return (
     <div>
       <Box
-      sx={(theme) => ({
-        padding: theme.spacing.xl,
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        width: '100%',
-        alignItems: 'center',
-      })
-      }
+        sx={(theme) => ({
+          padding: theme.spacing.xl,
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          width: '100%',
+          alignItems: 'center',
+        })
+        }
       >
-      <Title order={1} style={{ fontSize: '60px'}}>Elon Musk's Texts</Title>
-      <Text style={{ marginBottom: '2rem'}}>The private exchanges of the man who almost bought Twitter.</Text>
-      <Messages/>
-      </Box>
+        <Messages />
+        <div className='sideBar'>
+          <div className='iconWrapper' onClick={() => setModalOpened(true)}>
+            <IconQuestionMark size={50} color="#1a1b1e" />
+          </div>
+          <div 
+          className='iconWrapper' 
+          onClick={() => openInNewTab('https://github.com/JusGu/elon-musks-texts')}
+          style={{background: '#c1c2c5'}}
+          >
+            <IconBrandGithub size={50} color="#1a1b1e" />
+          </div>
 
+        </div>
+
+
+      </Box>
+      <Modal
+        title="About"
+        opened={modalOpened}
+        onClose={() => setModalOpened(false)}
+        size={600}
+      >
+        <Accordion
+          chevron={<IconChevronRight size={16} />}
+          styles={{
+            chevron: {
+              '&[data-rotate]': {
+                transform: 'rotate(90deg)',
+              },
+            },
+          }}
+        >
+          <Accordion.Item title="What is this?" value='1'>
+            <Accordion.Control>Background</Accordion.Control>
+            <Accordion.Panel>On Sept 29th 2022, Delaware’s Court of Chancery released hundreds of text messages to and from Elon Musk. However, Delaware’s Court of Chancery put all these text on a table that was printed, redacted and scanned rendering them basically unreadable. I spent days scanning through these documents to build this website so that you can read these text messages like actual text messages.</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item title="What is this website?" value='2'>
+            <Accordion.Control>What is this website?</Accordion.Control>
+            <Accordion.Panel>This website contains all of the text messages from the Court of Chancery’s release. The text messages are presented in an easily readable format and you can search for specific people.</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item title="Why did you do this?" value='3'>
+            <Accordion.Control>Why did you do this?</Accordion.Control>
+            <Accordion.Panel>I built this website because I think it’s important for people to be able to read these text messages in their entirety and not just snippets that are chosen by the media. I also think it’s important for people to be able to see the context in which they were used.</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item title="How did you do this?" value='4'>
+            <Accordion.Control>How did you do this?</Accordion.Control>
+            <Accordion.Panel>I used a combination of OCR (Optical Character Recognition) and manual transcription to convert the scanned images of the text messages into text that can be easily read.</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item title="Can I search for people?" value='5'>
+            <Accordion.Control>Can I search for people?</Accordion.Control>
+            <Accordion.Panel>Yes! There is a search bar at the top of the page where you can search for specific people?</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item title="For Media: how can I share this website?" value='6'>
+            <Accordion.Control>For Media: how can I share this website?</Accordion.Control>
+            <Accordion.Panel>Please email me at <a style={{ color: 'white' }} href="mailto:jjwgu@uwaterloo.ca">jjwgu@uwaterloo</a>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </Modal>
     </div>
   );
 }
